b0c3862100b98a1f210be14a88c82d06bfaddc00
ao.wang <luck.wang@11bee.com>
2019-02-13 21:18:56 +0800

    配置了一些eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
     /**
      * 启用的规则及其各自的错误级别，规则的分组和官网的分组保持一致 (http://eslint.cn/docs/rules/#nodejs-and-commonjs)
      * 参考：https://github.com/standard/eslint-config-standard/blob/master/eslintrc.json
+     * 该写这个了： no-mixed-operators
      */
     "rules": {
         /**
@@ -49,6 +50,9 @@ module.exports = {
 
         // 禁用 console
         "no-console": "off",
+        "no-extra-parens": ["error", "functions"],
+        // 禁止 function 声明出现在嵌套的语句块中
+        "no-inner-declarations": ["error", "functions"],
 
         /**
          * Best Practices
@@ -61,6 +65,24 @@ module.exports = {
         "curly": "error",
         // 表达式中的点号操作符应该和对象部分在同一行
         "dot-location": ["error", "object"],
+        // 要求使用 === 和 !==
+        "eqeqeq": ["error", "always"],
+        // 禁用 caller 或 callee
+        "no-caller": "error",
+        // 禁止扩展原生对象
+        "no-extend-native": "error",
+        // 禁止不必要的函数绑定
+        "no-extra-bind": "error",
+        // 禁止浮点小数，此规则目的在于消除浮动小数点，并且，当数值的小数点之前或之后缺少数字时，该规则将发出警告
+        "no-floating-decimal": "error",
+        // 禁用隐式的eval()
+        "no-implied-eval": "error",
+        // 禁用迭代器
+        "no-iterator": "error",
+        // 禁用标签语句
+        "no-labels": ["error", { "allowLoop": false, "allowSwitch": false }],
+        // 禁用不必要的嵌套块
+        "no-lone-blocks": "error",
 
         /**
          * Strict Mode
@@ -75,10 +97,16 @@ module.exports = {
          * 这些规则与变量声明有关：
          */
 
+        // 禁用与变量同名的标签
+        "no-label-var": "error",
+
         /**
          * Node.js and CommonJS
          * 这些规则是关于Node.js 或 在浏览器中使用CommonJS 的：
          */
+
+        // 在 Node.js 中，最普遍的处理异步行为是回调模式，这个模式期望一个 Error 对象或 null 作为回调的第一个参数。
+        "handle-callback-err": ["error", "^(err|error)$" ],
 
         /**
          * Stylistic Issues
@@ -105,10 +133,35 @@ module.exports = {
         "comma-style": ["error", "last"],
         // 强制文件末尾不要有换行符
         "eol-last": ["error", "never"],
-        // 强制使用一致的缩进风格：4个空格。
-        "indent": ["error", 4],
+        // 禁止在函数标识符和其调用之间有空格
+        "func-call-spacing": ["error", "never"],
+        // 强制使用一致的缩进风格：4个空格
+        "indent": ["error", 4, {
+            "SwitchCase": 1,
+            "VariableDeclarator": 1,
+            "outerIIFEBody": 1,
+            "MemberExpression": 1,
+            "FunctionDeclaration": { "parameters": 1, "body": 1 },
+            "FunctionExpression": { "parameters": 1, "body": 1 },
+            "CallExpression": { "arguments": 1 },
+            "ArrayExpression": 1,
+            "ObjectExpression": 1,
+            "ImportDeclaration": 1,
+            "flatTernaryExpressions": false,
+            "ignoreComments": false
+        }],
+        // 要求对象字面量中的冒号和值都水平对齐
+        "key-spacing": ["error", { "align": "colon" }],
+        // 要求在关键字之前至少有一个空格，要求在关键字之后至少有一个空格
+        "keyword-spacing": ["error", { "before": true, "after": true }],
         // 强制使用一致的换行符风格，强制使用 Unix 换行符
         "linebreak-style": ["error", "unix"],
+        // 要求调用 new 操作符时有首字母大写的函数，要求调用首字母大写的函数时有 new 操作符
+        "new-cap": ["error", { "newIsCap": true, "capIsNew": false }],
+        // 要求调用无参构造函数时带括号
+        "new-parens": "error",
+        // 由于单参数的陷阱，和全局范围的 Array 可能被重定义，通常不允许使用 Array的构造函数来创建数组。唯一的例外是通过给构造函数传入指定的一个数值来创建稀疏数组。
+        "no-array-constructor": "error",
         // 要求尽可能地使用单引号
         "quotes": ["error", "single"],
         // 要求在语句末尾使用分号
@@ -125,5 +178,7 @@ module.exports = {
         "arrow-parens": ["error", "always"],
         // 要求箭头函数的箭头之前或之后有空格
         "arrow-spacing": ["error", { "before": true, "after": true }],
+        // 强制 generator 函数中 * 号周围使用一致的空格
+        "generator-star-spacing": ["error", { "before": true, "after": true }],
     }
 };
