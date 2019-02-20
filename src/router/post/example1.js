29d1c0d758c7afbfb6ab7c78a391d4fd72756763
ao.wang <luck.wang@11bee.com>
2019-02-20 17:58:54 +0800

    为express的router提供数据存储的方法

--- src/router/post/example1.js
+++ src/router/post/example1.js
@@ -6,10 +6,17 @@
 let express = require('express');
 let router = express.Router();
 
+// 方法
+import { setRouterData, getRouterData, } from '../../util/router';
+
 // lodash
 import _get from 'lodash/get';
 
 router.post('/example1', function(req, res) {
+    // 测试setRouterData, getRouterData函数
+    setRouterData(res, 123);
+    console.log(`测试setRouterData: ${getRouterData(res)}`);
+
     let body = _get(req, 'body') || {};
     res.json(body);
 });
---
+++ src/util/router.js
@@ -0,0 +1,34 @@
+/**
+ * @file express路由传递参数的方法
+ * @author automation13 <1271547283@qq.com>
+ */
+
+const KEY = 'storeData';
+
+// lodash
+import _set from 'lodash/set';
+import _get from 'lodash/get';
+
+export {
+    setRouterData, // 向express的response对象中存数据
+    getRouterData, // 取出express的response对象中存数据
+};
+
+/**
+ * 向express的response对象中存数据
+ * @param { object } res express的response对象，必传
+ * @param { * } data 待存数据
+ */
+function setRouterData(res, data = {}) {
+    _set(res, [KEY], data);
+}
+
+/**
+ * 取出express的response对象中存数据
+ * @param { object } res express的response对象，必传
+ * @returns {*|{}}
+ */
+function getRouterData(res) {
+    const routerData = _get(res, [KEY]) || {};
+    return routerData;
+}
