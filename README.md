34efdba6bb7c89a1838eaca9c04ce3ba638d97b8
ao.wang <luck.wang@11bee.com>
2019-03-04 15:43:02 +0800

    引入express模板引擎hbs

--- README.md
+++ README.md
@@ -23,6 +23,8 @@ express简单的服务器
 7、body-parser       express插件，解析http的body
 8、cookie-parser     express插件，解析cookie
 9、express-session   express插件（官网插件名：session），解析express
+10、hbs              express模板引擎（https://github.com/pillarjs/hbs）
+
 
 代码规范：
 1、代码注释规范：    JSDoc（https://www.css88.com/doc/jsdoc/tags-description.html）
--- src/app.js
+++ src/app.js
@@ -26,6 +26,8 @@ app.set('trust proxy', 1); // Indicates the app is behind a front-facing proxy, 
  */
 // 通过如下代码就可以将 static 目录下的图片、CSS 文件、JavaScript 文件对外开放访问了，例如通过如下URL访问该目录下react-life.png图片：http://localhost:3000/static/images/react-life.png
 app.use('/static', express.static(path.join(__dirname, '../static')));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'hbs'); // express模板引擎：handlebarsjs
 app.use('/', router); // express路由
 
 /**
