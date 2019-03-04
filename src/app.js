48883dbcee8c63d1b76da8bbd49b971113d82932
ao.wang <luck.wang@11bee.com>
2019-03-04 11:36:28 +0800

    路由的设置从express的设置中抽出来放到router组件中

--- src/app.js
+++ src/app.js
@@ -9,9 +9,6 @@ import path from 'path';
 
 // express中间件
 import router from './router';
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
 
 // 服务器配置参数
 import { SERVER_PORT, } from './config/express';
@@ -29,27 +26,11 @@ app.set('trust proxy', 1); // Indicates the app is behind a front-facing proxy, 
  */
 // 通过如下代码就可以将 static 目录下的图片、CSS 文件、JavaScript 文件对外开放访问了，例如通过如下URL访问该目录下react-life.png图片：http://localhost:3000/static/images/react-life.png
 app.use('/static', express.static(path.join(__dirname, '../static')));
-// express中间件
-app.use(cookieParser()); // cookie解析的中间件
-app.use(session({ // Create a session middleware with the given options.
-    name             : 'connect.sid', // 使用默认sessionID
-    secret           : ['recommand 128 bytes random string', 'secretKey1', 'secretKey2'], // Required option.This is the secret used to sign the session ID cookie.
-    resave           : true,
-    saveUninitialized: true,
-    rolling          : true, // true：每次请求中超时时间都会重置，保证session一直不失效；false：不重置超时时间，即使持续请求，到时间后session同样失效
-    cookie           : { // default value is { path: '/', httpOnly: true, secure: false, maxAge: null }.
-        secure  : false, // 插件官网建议生产换件设为true. Using cookie-parser may result in issues if the secret is not the same between this module and cookie-parser.(http://www.expressjs.com.cn/en/resources/middleware/session.html)
-        httpOnly: true,
-        maxAge  : 24 * 60 * 60 * 1000, // 过期时间：1day
-    },
-}));
-app.use(bodyParser.json()); // create application/json parser
-app.use(bodyParser.urlencoded({ extended: true, })); // create application/x-www-form-urlencoded parser。（The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true)）（http://www.expressjs.com.cn/en/resources/middleware/body-parser.html）
 app.use('/', router); // express路由
 
 /**
  * @description 启动express服务器，监听端口号为：SERVER_PORT
  */
 app.listen(SERVER_PORT, () => {
-    console.log(`[server start information] express server starts to listen port ${SERVER_PORT}`);
+    console.log(`[my server start information] express server starts to listen port ${SERVER_PORT}`);
 });
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,28 @@
 let express = require('express');
 let router = express.Router();
 
+// router使用到的express中间件
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+// express中间件初始化
+router.use(cookieParser()); // cookie解析的中间件
+router.use(session({ // Create a session middleware with the given options.
+    name             : 'connect.sid', // 使用默认sessionID
+    secret           : ['recommand 128 bytes random string', 'secretKey1', 'secretKey2'], // Required option.This is the secret used to sign the session ID cookie.
+    resave           : true,
+    saveUninitialized: true,
+    rolling          : true, // true：每次请求中超时时间都会重置，保证session一直不失效；false：不重置超时时间，即使持续请求，到时间后session同样失效
+    cookie           : { // default value is { path: '/', httpOnly: true, secure: false, maxAge: null }.
+        secure  : false, // 插件官网建议生产换件设为true. Using cookie-parser may result in issues if the secret is not the same between this module and cookie-parser.(http://www.expressjs.com.cn/en/resources/middleware/session.html)
+        httpOnly: true,
+        maxAge  : 24 * 60 * 60 * 1000, // 过期时间：1day
+    },
+}));
+router.use(bodyParser.json()); // create application/json parser
+router.use(bodyParser.urlencoded({ extended: true, })); // create application/x-www-form-urlencoded parser。（The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true)）（http://www.expressjs.com.cn/en/resources/middleware/body-parser.html）
+
 // 导入路由
 import routerStart from './start'; // 所有业务路由之前处理数据
 import routerGet from './get'; // get请求
